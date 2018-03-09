ea833ab2cf4a052f1271ffecffadffc4d415dacf
Jeremy <jnebenionquit@gmail.com>
2018-03-08 23:55:17 -0500

    progress 2

--- js/gameScript.js
+++ js/gameScript.js
@@ -1,3 +1,20 @@
+//Key Handler
+document.addEventListener('keydown', function(event) {
+    if(event.keyCode == 37 && player.xPos != 0) {
+      player.xPos -= 1;
+    }
+    else if(event.keyCode == 38 && player.yPos != 0) {
+      player.yPos -= 1;
+    }
+    else if(event.keyCode == 39 && player.xPos != boardWidth - 1) {
+      player.xPos += 1;
+    }
+    else if(event.keyCode == 40 && player.yPos != boardHeight - 1) {
+      player.yPos += 1;
+    }
+    render();
+});
+
 //Image array
 var images = [];
 function load() {
@@ -11,29 +28,67 @@ load(
   "images/rect-temp.png",
   "images/rect-temp-player.png",
   "images/rect-temp-goal.png",
-  "images/rect-temp-enemy.png"
+  "images/rect-temp-enemy.png",
+  "images/rect-temp-forest.png",
+  "images/rect-temp-boss.png",
+  "images/rect-temp-mtn.png"
 )
 
-//Canvas
-var canvas = document.getElementById("gameBoard");
-var cx = canvas.getContext("2d");
+var canvas = document.getElementById('gameBoard');
+if (canvas.getContext) {
+  var cx = canvas.getContext('2d');
+}
 
 //Board size - Could be dynamic
 var boardWidth = 10;
 var boardHeight = 10;
-var cellSize = 64;
+const cellSize = 64;
 
 //Map code
-var defaultCode = 0;
-var playerCode = 1;
-var goalCode = 2;
-var enemyCode = 3;
+const emptyTile = 0;
+const playerTile = 1;
+const goalTile = 2;
+const enemyTile = 3;
+const forestTile = 4;
+const bossTile = 5;
 
 //Object vars
 var board;
 var player;
 var enemy;
+var boss;
 var goal;
+
+goal = {
+  xPos : boardWidth - 1,
+  yPos : 0
+}
+
+player = {
+  health : 6,
+  gold : 0,
+  rations : 10,
+  damage : 1,
+  xPos : 0,
+  yPos : boardHeight - 1
+};
+
+enemy = {
+  health : 2,
+  gold : 2,
+  rations : 2,
+  damage : 2,
+  xPos : 0,
+  yPos : 0
+};
+
+boss = {
+  health : 10,
+  gold : 100,
+  damage : 3,
+  xPos : 0,
+  yPos : 0
+};
 
 //Setup board
 board = new Array(boardWidth);
@@ -41,53 +96,61 @@ for (var i = 0; i < boardWidth; i++) {
   board[i] = new Array(boardHeight);
 }
 
-//Fill with zeros
-for (var i = 0; i < boardWidth; i++) {
-  for (var j = 0; j < boardHeight; j++) {
-      board[i][j] = defaultCode;
-  }
-}
+//Setup static map
+board = [[0,4,0,0,0,0,4,0,0,0],
+        [0,0,4,0,0,0,0,0,0,4],
+        [0,4,0,4,10,10,10,10,10,10],
+        [0,4,0,10,10,10,10,4,10,10],
+        [10,10,0,0,4,0,0,6,4,10],
+        [10,10,10,4,0,0,0,0,10,10],
+        [10,10,10,10,0,0,0,10,10,10],
+        [4,4,10,10,10,10,4,4,10,10],
+        [4,0,0,0,0,0,0,4,4,4],
+        [0,0,0,0,0,0,0,4,4,4]];
 
 //Set player position and goal
-board[0][boardHeight-1] = playerCode;
-board[boardWidth-1][0] = goalCode;
+board[boardHeight-1][0] = playerTile;
+board[0][boardWidth-1] = goalTile;
 
 //Generate enemy positions randomly
-var randX, randY;
-for (numEnemy = 0; numEnemy < 8; numEnemy++) {
-  randX = Math.floor(Math.random()*10);
-  randY = Math.floor(Math.random()*10);
+function generateEnemies() {
+  var randX, randY;
+  for (numEnemy = 0; numEnemy < 8; numEnemy++) {
+    randX = Math.floor(Math.random()*10);
+    randY = Math.floor(Math.random()*10);
+    console.log(board[randX][randY]);
 
-  //Check if an empty tile, if not, try again until empty
-  if (board[randX][randY] == 0) {
-    board[randX][randY] = 3;
-  } else {
-    numEnemy--;
+    //Check if an empty tile, if not, try again until empty
+    if (board[randX][randY] == 0) {
+      board[randX][randY] = enemyTile;
+    } else {
+      numEnemy--;
+    }
   }
 }
 
-//Using jQuery, wait to load images to screen
-$(function() {
-  for (var i = 0; i < boardWidth; i++) {
-    for (var j = 0; j < boardHeight; j++) {
-      if (board[i][j] == 0) {
+function render() {
+  cx.clearRect(0, 0, canvas.width, canvas.height);
+  for (var i = 0; i < 10; i++) {
+    for (var j = 0; j < 10; j++) {
+      if (board[j][i] == 0) {
         cx.drawImage(images[0], i*cellSize, j*cellSize);
-        console.log("Success! (0)");
-      }
-      else if (board[i][j] == 1) {
-        cx.drawImage(images[1], i*cellSize, j*cellSize);
-        console.log("Success! (1)");
-      }
-      else if (board[i][j] == 2) {
+      } else if (board[j][i] == 2) {
         cx.drawImage(images[2], i*cellSize, j*cellSize);
-        console.log("Success! (2)");
-      }
-      else if (board[i][j] == 3) {
+      } else if (board[j][i] == 3) {
         cx.drawImage(images[3], i*cellSize, j*cellSize);
-        console.log("Success! (3)");
-      } else {
-        console.log("Error!");
+      } else if (board[j][i] == 4) {
+        cx.drawImage(images[4], i*cellSize, j*cellSize);
+      } else if (board[j][i] == 10) {
+        cx.drawImage(images[6], i*cellSize, j*cellSize);
       }
     }
   }
+  cx.drawImage(images[1], player.xPos*cellSize, player.yPos*cellSize);
+};
+
+//Using jQuery, wait to load images to screen
+$(function() {
+  generateEnemies();
+  render();
 });
